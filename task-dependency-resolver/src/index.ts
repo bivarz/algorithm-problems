943ad37f7666bc0f2af9405951f4a73d1d68359e
Paulo Bivar Filho <53359269+bivarz@users.noreply.github.com>
2026-02-10 11:54:06 -0300

    Change blocked task from 'c' to 't2'

--- task-dependency-resolver/src/index.ts
+++ task-dependency-resolver/src/index.ts
@@ -92,4 +92,4 @@ export function getBlockedTasks(tasks: Task[], completedIds: string[]) {
 
 resolveOrder(tasks);
 resolveOrder(cyclicTasks);
-getBlockedTasks(tasks, ['c']);
+getBlockedTasks(tasks, ['t2']);
